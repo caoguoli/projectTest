c1fb60aee2ffefd548f7065cef942b4c640ae700
cgh <congguanghui-sj@bestpay.com.cn>
2022-04-24 13:10:12 +0800

    commit by cgh copy code

--- src/pages/IKAHE-Toilet/views/home/index.vue
+++ src/pages/IKAHE-Toilet/views/home/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <span>我是第一行数据</span>
+    <span>我是第一行数据</span>
     <span>我添加了一行代码cgl1</span>
     <span>我添加了一行代码cgl1</span>
     <span>我添加了一行代码cgl1</span>
     <span>我添加了一行代码cgl1</span>
+
   </div>
 </template>
