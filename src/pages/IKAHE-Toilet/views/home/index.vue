ce3ff212df7f6ab0fecd0cf5963ab6601de6ce68
caoguoli <1914199496@qq.com>
2022-07-05 10:26:49 +0800

    修改了部分代码

--- src/pages/IKAHE-Toilet/views/home/index.vue
+++ src/pages/IKAHE-Toilet/views/home/index.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
     <span>我是第一行数据</span>
-    <span>我是第一行数据</span>
-    <span>我添加了一行代码cgl1</span>
-    <span>今天天气真好</span>
-    <span>我添加了一行代码cgl1</span>
-    <span>我添加了一行代码cgl1</span>
-    <span>我添加了一行代码cgl1</span>
-    <p>🐛🐛是个大坏蛋</p>
-    <span>我添加了一行代码cgl1</span>
-
   </div>
 </template>
